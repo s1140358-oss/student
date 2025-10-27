8b4253978194751a012c0b7df94b3e5961c8b9f5
s1140358-oss <s1140358@dmhs.kh.edu.tw>
2025-10-27 16:15:42 +0800

    Add files via upload

---
+++ 作業2hw2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age = 15;
+    double height = 165.1;
+    std::string name = "洪子喬Ariel";
+    bool is_student = false;
+    std::cout <<  "姓名: " << name << std::endl;
+    std::cout <<  "身高: " << height << std::endl;
+    std::cout <<  "年齡: " << age << std::endl;
+    std::cout <<  "是否為學生: " << is_student << std::endl;
+    
+    return 0;
+}
